0f3c3764c2db525cf83fb3824f8533cda45b76b5
Dave Lester <davelester@gmail.com>
2012-10-20 16:04:27 -0700

    Initial skeleton of Flask app, plus configuration for DB and gitignore.

---
+++ app.py
@@ -0,0 +1,28 @@
+from tornado.database import Connection
+from flask import Flask, g, render_template
+app = Flask(__name__)
+import config
+
+@app.before_request
+def connect_db():
+    g.db = Connection(config.SPOTIFONT_DB_HOST,
+                      config.SPOTIFONT_DB_NAME,
+                      config.SPOTIFONT_DB_USER,
+                      config.SPOTIFONT_DB_PASSWD)
+
+@app.after_request
+def close_connection(response):
+    g.db.close()
+    return response
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/search/<query>")
+def search(query):
+	fontnames = g.db.iter("select f.* FROM Fonts AS f")
+	return render_template("search.html", fontnames=fontnames)
+
+if __name__ == "__main__":
+    app.run()
