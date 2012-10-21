0f210ad75f6f10de423d564f4caa4c66f954bc15
Aijia Yan <aijiay@berkeley.edu>
2012-10-21 06:14:38 -0700

    add a default account authentication and some changes in index.html

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from tornado.database import Connection
-from flask import request
 from os import environ
-from flask import Flask, g, render_template, Response
-app = Flask(__name__)
+from flask import Flask, g, render_template, Response, url_for, request, session, flash, redirect
 import config
 
+
+#Configuration, the default log-in name is username = admin, and password = default
+USERNAME = "admin"
+PASSWORD = "default"
+SECRET_KEY = 'development key'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 app.debug = True
 
@@ -51,25 +58,41 @@ def font(fontname):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != "aijiay":
-            error = 'Invalid username'
-            return render_template('login.html', error=error)
-        elif request.form['password'] != "Zz064168919!":
-            error = 'Invalid password'
-            return render_template('login.html', error=error)
-        '''else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('/'))'''
-    return render_template('login.html', error=error)
+  error = None
+  if request.method == 'POST':
+    if request.form['username'] != app.config['USERNAME']: #where USERNAME = admin
+      error = 'Invalid username'
+    elif request.form['password'] != app.config['PASSWORD']:#where PASSWORD = default
+      error = 'Invalid password'
+    else:
+      session['logged_in'] = True
+      flash('You were logged in')
+      return redirect("/")
+  return render_template('login.html', error=error)
+
 
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
-    return redirect(url_for('show_entries'))
+    return redirect("/login")
+
+
+#this is to test if the table is returning the correct usernames
+@app.route('/fetch')
+def fetch():
+  usernames = list(g.db.iter("SELECT username, password FROM Users"))
+  return render_template("test.html", user=usernames)
+
+
+#the following method should get new user to register, though I am not sure how to INSERT INTO database
+'''@app.route('/register', methods=['GET', 'POST'])
+def register():
+  user = request.form.get['username']
+  pw = request.form.get['password']
+  g.db("INSERT INTO Users (id, username, password) VALUES (3, user, pw)")
+
+  return render_template("register.html", user=user, password=pw)'''
 
 if __name__ == "__main__":
     app.run()
