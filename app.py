362c01e2c56dc2e9a0b1073754b9cbfd98338723
Dave Lester <davelester@gmail.com>
2012-10-20 23:43:46 -0700

    Updates search API, so it returns application/json mimetype.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from tornado.database import Connection
-from flask import Flask, g, render_template
+from flask import Flask, g, render_template, Response
 app = Flask(__name__)
 import config
 
@@ -22,8 +22,18 @@ def index():
 @app.route("/search/<query>")
 def search(query):
 	fonts = g.db.iter("SELECT DISTINCT f.font_name, c.controlled_name FROM Fonts AS f, Tags as t, Tag_Links as tl, Controlled as c WHERE c.font_id=f.id AND f.id=tl.font_id AND tl.tag_id=t.id AND t.tag_name='" + query + "'")
-	return render_template("search.html", fonts=fonts)
+	
+	ret = '['	
+	for font in fonts:
+		ret+='{"fontname": "' + font.font_name +'", "controlled": "' + font.controlled_name +'"}'
+	ret += ']'
 
+	resp = Response(response=ret,
+					status=200,
+					mimetype="application/json")
+
+	return resp
+	
 @app.route("/font/<fontname>")
 def font(fontname):
 	# todo: should pull only unique tags, and also return the count of the times a tag appears for a given font
